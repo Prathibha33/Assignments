d42114e40e03189a0636744f76f9efd1048976dc
Prathibha33 <129115741+Prathibha33@users.noreply.github.com>
2023-03-31 01:29:36 +0100

    Update Assignment 4-ChangeNegToPos.java

--- week1Day2 Assignments/Assignment 4-ChangeNegToPos.java
+++ week1Day2 Assignments/Assignment 4-ChangeNegToPos.java
@@ -2,14 +2,14 @@ package week1Day2;
 
 public class ChangeNegToPos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		
 		int num=-40;
 		if(num<=0) {
 			num = num*-1;
 
 		}
-		System.out.println("The number -40 is converted to " + num);
+System.out.println("The number -40 is converted to " + num);
 	}
 
 }
