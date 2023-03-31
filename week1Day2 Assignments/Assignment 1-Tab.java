e1bebb6ce28f86d5e3a825c718a158ddaf3d6f2e
Prathibha33 <129115741+Prathibha33@users.noreply.github.com>
2023-03-31 01:24:19 +0100

    Update and rename Tab.java to Assignment 1-Tab.java

---
+++ week1Day2 Assignments/Assignment 1-Tab.java
@@ -0,0 +1,24 @@
+package week1Day2;
+
+public class Tab {
+	
+public int tabPrice(int tabPrice) {
+return tabPrice;
+}
+public String tabBrand(String brandName) {
+return brandName;
+}
+        public static void main(String[] args) {
+	Tab tab = new Tab();
+        tab.tabPrice(0);
+        tab.tabBrand(null);
+        String brand = tab.tabBrand("Samsumg");
+        int price = tab.tabPrice(20000);
+        System.out.println("Tab brand is : " + brand);
+	System.out.println("Tab price is : " + price);
+	}
+
+	
+	}
+
+
